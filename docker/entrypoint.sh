04e2749f56ea70f5b1b27f527fef75306eded057
Mike Sardonini <sardonini.michael@aurora.aero>
2022-09-02 14:43:20 -0700

    forgot the hash bang

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+
+echo "hello!"
+
 mkdir /github/workspace/build
 
 cmake -D CMAKE_BUILD_TYPE=Release -S /github/workspace -B /github/workspace/build
 make -j4 -C /github/workspace/build
+
+
